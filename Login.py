9b340e77e275728801dc87f754f13067e6a3f49d
achyut3598 <31666235+achyut3598@users.noreply.github.com>
2019-07-13 22:28:34 -0400

    Signup and Login pages and an important note
    Signup is functional in the sense that it stores data in a text file. Login doesn't do anything right now. Once we have the database setup, we can decode and compare the passwords
    
    Note:
    https://github.com/Electron-Cash/Electron-Cash/issues/892 Follow the solution here to fix PyQt sip issues

---
+++ Login.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\Achyut\Project\Login.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+
+    def login(self):
+        username = self.lineEdit.text()
+        password = self.lineEdit_2.text()
+        #Compare the passwords
+        #Create a box seaying login successful or something and redirect to Dashboard
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(280, 60, 251, 61))
+        font = QtGui.QFont()
+        font.setPointSize(25)
+        self.label.setFont(font)
+        self.label.setScaledContents(False)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(230, 200, 121, 31))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(230, 260, 121, 21))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(450, 199, 113, 31))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(450, 250, 113, 31))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(220, 120, 351, 51))
+        font = QtGui.QFont()
+        font.setPointSize(21)
+        self.label_7.setFont(font)
+        self.label_7.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_7.setObjectName("label_7")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(320, 320, 171, 41))
+        font = QtGui.QFont()
+        font.setPointSize(21)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Travel Scheduler"))
+        self.label_2.setText(_translate("MainWindow", "Username"))
+        self.label_3.setText(_translate("MainWindow", "Password"))
+        self.label_7.setText(_translate("MainWindow", "Login"))
+        self.pushButton.setText(_translate("MainWindow", "Submit"))
+        self.pushButton.clicked.connect(self.login)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
